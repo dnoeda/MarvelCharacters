fd1ddf599d5c8dde78d9a95cd1a55051570baab9
David Noeda <dnoeda@gmail.com>
2020-10-17 17:47:30 +0200

    Setup navigation bar style

--- MarvelCharacters/SceneDelegate.swift
+++ MarvelCharacters/SceneDelegate.swift
@@ -16,9 +16,33 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
       // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
       // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
       // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-      guard let _ = (scene as? UIWindowScene) else { return }
+      guard let windowScene = (scene as? UIWindowScene) else { return }
+      
+      let navigationController = UINavigationController(rootViewController: UIViewController())
+      
+      setupNavigationBar(navigationController: navigationController)
+      
+      let window = UIWindow(windowScene: windowScene)
+      window.rootViewController = navigationController
+      self.window = window
+      window.makeKeyAndVisible()
    }
 
+   private func setupNavigationBar(navigationController: UINavigationController) {
+      let navigationBar = navigationController.navigationBar
+      
+      navigationBar.isTranslucent = true
+      navigationBar.setBackgroundImage(UIImage(), for: .default)
+      navigationBar.shadowImage = UIImage()
+      navigationBar.backgroundColor = .clear
+      
+      navigationBar.topItem?.backBarButtonItem = UIBarButtonItem(title: " ", style: .plain, target: nil, action: nil)
+      navigationBar.topItem?.title = " "
+      
+      navigationController.navigationBar.tintColor = UIColor.black
+      navigationController.setNavigationBarHidden(false, animated: true)
+   }
+   
    func sceneDidDisconnect(_ scene: UIScene) {
       // Called as the scene is being released by the system.
       // This occurs shortly after the scene enters the background, or when its session is discarded.
